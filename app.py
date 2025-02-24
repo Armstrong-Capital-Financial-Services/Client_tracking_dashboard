bfae66fe473075d3106ca4cfe381c04cbd54d73f
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-02-24 15:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import streamlit as st
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
